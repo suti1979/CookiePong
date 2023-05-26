b88a37641ca637f1cf998b0e836ae1a9af590d16
Andras Zold <andras.zold@digitalthinkers.com>
2023-05-26 11:41:47 +0200

    added collisions and death count :)

--- CookiePong/ContentView.swift
+++ CookiePong/ContentView.swift
@@ -5,18 +5,33 @@ struct ContentView: View {
     @State private var ballPosition = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
     @State private var ballVelocity = CGVector(dx: 3.0, dy: -3.0)
     @State private var isGamePaused = false
+    @State private var currentFruit = "🍏"
+    @State private var fruitPosition = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
+    
+    @State private var deathCount = 0
+    @State private var hitCount = -1
+    
     private let racketWidth: CGFloat = 100
     private let racketHeight: CGFloat = 20
-    private let ballRadius: CGFloat = 10
+    private let ballRadius: CGFloat = 18
+    
+    private let fruits: [String] = ["🍔","🍕","🍗","🥩","🌮","🍟"]
     
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 Color.green
                 
-                // Draw the racket
-                Rectangle()
-                    .foregroundColor(.white)
+                Text("Hit: \(hitCount)").position(x:40, y:40)
+                Text("Death: \(deathCount)").position(x:40, y:80)
+                
+                Capsule()
+                    .fill(LinearGradient(
+                        gradient: .init(colors: [.brown, .black]),
+                        startPoint: .init(x: 0.5, y: 0.5),
+                        endPoint: .init(x: 0.5, y: 1)
+                    ))
+                
                     .frame(width: racketWidth, height: racketHeight)
                     .position(racketPosition)
                     .gesture(
@@ -27,35 +42,49 @@ struct ContentView: View {
                             }
                     )
                 
-                // Draw the ball
-                Circle()
+                Text("🍪")
+                    .font(.system(size: 36))
                     .foregroundColor(.white)
                     .frame(width: ballRadius * 2, height: ballRadius * 2)
                     .position(ballPosition)
                     .onAppear {
                         startAnimatingBall()
+                        fruitPosition = randomFruitPosition()
+                        currentFruit = randomFruit()
                     }
+                
+                
+                Text(currentFruit)
+                    .font(.system(size: 50))
+                    .position(fruitPosition)
+                
             }
             .edgesIgnoringSafeArea(.all)
             .alert(isPresented: $isGamePaused) {
                 Alert(
-                    title: Text("Game Over"),
-                    message: Text("Tap the screen to continue"),
-                    dismissButton: .default(Text("Play")) {
+                    title: Text("Don't Panic"),
+                    message: Text("Press cookie continue"),
+                    dismissButton: .default(Text("🍪")) {
                         startAnimatingBall()
                     }
                 )
             }
-//            .onTapGesture {
-//                if isGamePaused {
-//                    startAnimatingBall()
-//                }
-//            }
         }
     }
     
     private func startAnimatingBall() {
-        let timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
+        // Reset ball position and velocity
+        ballPosition = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
+        ballVelocity = CGVector(dx: 3.0, dy: -3.0)
+        
+        var timer: Timer?
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
+            if isGamePaused {
+                timer?.invalidate()
+                timer = nil
+                return
+            }
+            
             ballPosition.x += ballVelocity.dx
             ballPosition.y += ballVelocity.dy
             
@@ -75,10 +104,32 @@ struct ContentView: View {
             // Check if ball goes below the screen
             if ballPosition.y > UIScreen.main.bounds.height {
                 isGamePaused = true
-                
+                deathCount += 1
             }
+            
+            if fruitHit(ballPosition: ballPosition, fruitPosition: fruitPosition) {
+                hitCount += 1
+                fruitPosition = randomFruitPosition()
+                currentFruit = randomFruit()
+            }
+            
         }
-        timer.fire()
+        timer?.fire()
+    }
+    
+    private func fruitHit(ballPosition: CGPoint, fruitPosition: CGPoint) -> Bool {
+        let distance = sqrt(pow(ballPosition.x - fruitPosition.x, 2) + pow(ballPosition.y - fruitPosition.y, 2))
+        return distance <= ballRadius
+    }
+    
+    private func randomFruit() -> String {
+        return fruits.randomElement() ?? ""
+    }
+    
+    private func randomFruitPosition() -> CGPoint {
+        let x = CGFloat.random(in: 0...UIScreen.main.bounds.width)
+        let y = CGFloat.random(in: 0...UIScreen.main.bounds.height / 2)
+        return CGPoint(x: x, y: y)
     }
 }
 
