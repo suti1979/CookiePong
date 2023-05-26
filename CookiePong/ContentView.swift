4f032d5e34be4e8ffa627bf113a0eab478c68553
Andras Zold <andras.zold@digitalthinkers.com>
2023-05-26 09:11:25 +0200

    Initial Commit

---
+++ CookiePong/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CookiePong
+//
+//  Created by Zöld András on 2023. 05. 26..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CookiePong/CookiePongApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookiePongApp.swift
+//  CookiePong
+//
+//  Created by Zöld András on 2023. 05. 26..
+//
+
+import SwiftUI
+
+@main
+struct CookiePongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
